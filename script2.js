8f5ad6e9457f024220a5bd3153958afac55d4edf
Danny Huang <kuanhsuh@gmail.com>
2017-03-10 11:52:07 +0800

    Refactor model code

--- script2.js
+++ script2.js
@@ -1,9 +1,10 @@
-function playSequence(count){
+function playSequence(count) {
   var i = 0
-  function play(){
+
+  function play() {
     console.log(model.currentDisplay[i]);
     i++;
-    if( i < count+1){
+    if (i < count + 1) {
       setTimeout(play, 1000)
     }
   }
@@ -17,12 +18,12 @@ var model = {
   isOn: false,
   isStrict: false,
 
-  game: function(){
-    if(this.isOn){
+  game: function() {
+    if (this.isOn) {
       this.reset()
     }
   },
-  reset: function(){
+  reset: function() {
     this.randomSeries = [];
     this.generateSeries();
     this.currentDisplay = [this.randomSeries[0]];
@@ -30,33 +31,31 @@ var model = {
     this.count = 0;
     playSequence(this.count)
   },
-  generateSeries: function(){
+  generateSeries: function() {
     var num = 5;
-    for(var i =0; i < num; i++){
+    for (var i = 0; i < num; i++) {
       this.randomSeries.push(Math.floor(Math.random() * 4) + 1);
     }
   },
-  userMove: function(value){
+  userMove: function(value) {
     this.userInput.push(value)
     this.compareSequence(this.userInput.length - 1);
   },
-  compareSequence: function(element){
-    if(this.compare(this.currentDisplay[element], this.userInput[element])){
-      console.log('correct input please enter next input')
-      if(this.userInput.length === this.currentDisplay.length){
-        console.log('Enter correct sequence, now add one more')
+  compareSequence: function(element) {
+    if (this.compare(element)) {
+      if (this.isLastUserInput()) {
         this.userInput = [];
         this.count++
-        if(this.count === this.randomSeries.length){
-          console.log("you win")
-          this.reset()
-        } else {
-          this.currentDisplay.push(this.randomSeries[this.count])
-          playSequence(this.count)
-        }
+          if (this.isWon()) {
+            console.log("WON")
+            this.reset()
+          } else {
+            this.currentDisplay.push(this.randomSeries[this.count])
+            playSequence(this.count)
+          }
       }
     } else {
-      if(this.isStrict){
+      if (this.isStrict) {
         this.reset()
         console.log("strict mode")
       }
@@ -65,54 +64,13 @@ var model = {
       this.userInput = [];
     }
   },
-  compare: function(randomSeriesValue, userInputValue){
-    return userInputValue === randomSeriesValue;
+  compare: function(element) {
+    return this.currentDisplay[element] === this.userInput[element]
+  },
+  isLastUserInput: function() {
+    return this.userInput.length === this.currentDisplay.length
+  },
+  isWon: function() {
+    return this.count === this.randomSeries.length
   }
 }
-// function compare(randomSeriesValue, userInputValue){
-//   return userInputValue === randomSeriesValue;
-// }
-
-// function userMove(value){
-//   userInput.push(value)
-//   compareSequence(userInput.length - 1);
-// }
-
-// function compareSequence(element){
-//     if(compare(currentDisplay[element], userInput[element])){
-//       console.log('correct input please enter next input')
-//       if(userInput.length === currentDisplay.length){
-//         console.log('Enter correct sequence, now add one more')
-//         userInput = [];
-//         count++
-//         if(count === randomSeries.length){
-//           console.log("you win")
-//           reset()
-//         } else {
-//           currentDisplay.push(randomSeries[count])
-//           playSequence(count)
-//         }
-//       }
-//     } else {
-//       if(isStrict){
-//         reset()
-//         console.log("strict mode")
-//       }
-//       console.log('incorrect')
-//       playSequence(count)
-//       userInput = [];
-//     }
-// }
-//
-// function reset(){
-//   randomSeries = [];
-//   generateSeries();
-//   currentDisplay = [randomSeries[0]];
-//   userInput = [];
-//   count = 0;
-//   playSequence(count)
-// }
-//
-// function compare(randomSeriesValue, userInputValue){
-//   return userInputValue === randomSeriesValue;
-// }
